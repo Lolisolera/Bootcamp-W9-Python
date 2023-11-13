12ffd047e03dc32a50c11d362138b39cd724f480
Lola Marquez <122627993+Lolisolera@users.noreply.github.com>
2023-11-13 15:09:18 +0000

    new files created

---
+++ Day1.py
@@ -0,0 +1,68 @@
+
+# Task 1
+# Exercise: use the print statement to print
+#print("Python is installed and working correctly in Visual Studio!")
+
+# Your name
+#print("My name is Lola")
+# Your address
+##print("My address is Old Farm Road, London ")
+# Your interest(s)
+#print("I like music and making movies ")
+# Perform either addition or calculation with two numbers
+
+
+
+
+
+# Declaring variables
+#intro_message = "Python is installed and working correctly in Visual Studio!"
+#name = "Lola"
+#address = "Old Farm Road, London"
+#interests = "My interests include music and painting"
+
+# Displaying information using variables
+#print(intro_message)
+#print("My name is", name)
+#print("My address is", address)
+#print(interests)
+
+
+#num1 = int(input("Enter your first number: "))
+# JS number, Python = integer (WHOLE numbers), float (DECIMAL numbers e.g. 4.5)
+
+#num1 = int(input("Enter your first number: "))
+#num2 = int(input("Enter your second number: "))
+
+
+
+
+
+# Task 3: using input function
+# complete the rest of the code:
+#num2 = int ("Enter your second number: ")) 
+
+
+# Use a variable and invoke/call the input function to ask the user for a second number
+
+# add the two numbers
+##total = num1 + num2
+
+# print out the total of the two numbers
+#print(f"the total sum of {num1} + {num2} = {total}")
+
+
+
+# convert your name , interests and city variables to become dynamic
+#dynamic values  = values defined byt he user, so this can vary each time
+
+
+# Input for name, interests, and city
+name = input("Enter your name: ")
+interests = input("Enter your interests: ")
+city = input("Enter your city: ")
+
+# Displaying the entered information
+print("Name:", "name")
+print("Interests:", "interests")
+print("City:", "city ")
