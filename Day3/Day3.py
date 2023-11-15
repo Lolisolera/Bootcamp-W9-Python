e5099d22251010b9ef720c74cfa5e8d6ac8f5b4e
Lola Marquez <122627993+Lolisolera@users.noreply.github.com>
2023-11-15 14:34:06 +0000

    Update Day3.py

--- Day3/Day3.py
+++ Day3/Day3.py
@@ -15,8 +15,48 @@ def countdown(start, stop, step):
 countdown(11, 0, -1)
 
 
-# TIM'S SOLUTION:
+#TASK 1
 
-for findNumber in range(5, -1, -1):
-    print(findNumber)
-#5,4,3,2,1,0
+# Complete the code below to iterate through both lists, add comment to explain your code 
+
+highscore = [125, 63, 35, 12]
+#"block of code is missing here"
+
+for score in highscore: # Iterate through the highscore list
+
+    print(score)  # Print each score
+
+usersList = ["Anna", "Paul", "Joe", "Jane", "Anne", "Pauline", "Joan", "Janet"]
+#"block of code is missing here"
+for user in usersList: # Iterate through the usersList
+    print(user)   # Print each user
+
+
+#TASK2 
+# Debug and fix the multiplication program below 
+# add comment where you fix the bugs
+
+print("Welcome to the table quiz.\n")
+num = int(input("Enter a number: "))
+
+
+for i in range(1, 11): #Use range to define the range of values for i in the loop
+     # Indent the following lines to be inside the loop
+    answer = int(input(f" What is {i} x {num}? "))
+    print(f"the answer is {answer} ")
+
+      # Fix: Calculate the correct answer inside the loop
+       
+    correct = i * num
+
+    if answer == correct:
+        print("Correct")
+    else:
+        print(f"No, the answer is {correct}")
+  
+
+
+#--------------------------------
+
+
+
