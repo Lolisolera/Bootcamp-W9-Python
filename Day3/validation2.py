6c2346900e793a5747184402fba1348f8e1dc96c
Lola Marquez <122627993+Lolisolera@users.noreply.github.com>
2023-11-16 10:24:07 +0000

    Update validation2.py

--- Day3/validation2.py
+++ Day3/validation2.py
@@ -1,8 +1,16 @@
+
 name = input("Enter name: ").title()
 
-# Return True if the string is a lowercase string, False otherwise.
-if name.istitle():
+if name:
     print(f"Welcome {name}")
 else:
-    print(f"Your name {name} is not in title case")
-    exit()  # exit
+    print(f"Your name '{name}' is not in title case.")
+
+exit()  # exit
+
+
+# I've tested the above code and no matter what input type I enter, 
+# the output is always a welcoming message with my name.
+# BUT if I delete the .title() function for the first line, 
+# the "else" statement is activated!!! WHY???
+
